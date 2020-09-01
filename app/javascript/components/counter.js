ba44c53923f2475d46d8393b6a435286365c36f6
Liam Baker <liam.baker2310@gmail.com>
2020-09-01 14:28:51 +0900

    added footer

--- app/javascript/components/counter.js
+++ app/javascript/components/counter.js
@@ -3,15 +3,21 @@ $(upvoteCocktailCubes() {
 
   let count;
   cocktailCards.forEach((card) => {
-    card.addEventListener('click', (event) => {
-      const counter-text = card.querySelector('.cocktail-count')
-      const cube = card.querySelector('.fa-cubes');
+    const cube = card.querySelector('.fa-cubes');
+    const cocktailCount = card.querySelector('.cocktail-count');
+    cube.addEventListener('click', (event) => {
+      event.preventDefault();
       count = parseInt(cube.dataset.counter, 10);
       count += 1;
       cube.dataset.counter = count;
-      cocktail-cubes.innerText = count;
+      cocktailCount.innerHTML = `<i class="fas fa-cubes fa-2x" aria-hidden="true"></i> ${count}`;
+      $.ajax({
+        url: `cocktails/${card.id}/cubes`,
+        method: "PATCH",
+        data: { cocktail_id: card.id, new_count: count }
+      });
     });
-  });
+  })
 });
 
 export { upvoteCocktailCubes };
